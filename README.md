9362e228f2d79835ecbfdad23da1c5fcfa064c1d
suprstarrd <26027089+suprstarrd@users.noreply.github.com>
2025-11-28 04:27:51 +0000

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# LCSideStore
+# LiveContainer + SideStore repository
+This is an unofficial AltSource that allows you to use the latest LiveContainer nightly with the latest SideStore nightly.
+
+Disclaimer that while I am a member of the SideStore team, this is a **third-party** LiveContainer *and* SideStore build. You will not receive support for these builds: no one from the LiveContainer team or the SideStore team will help you. I make these builds for myself and myself only.
+
+## LiveContainer
+- LiveContainer is an app launcher (not emulator or hypervisor) that allows you to run iOS apps inside it without actually installing them.
+- Allows you to "install" unlimited apps (3 app/10 app id free developer account limit does not apply here) with only one app & app id. You can also have multiple versions of an app installed with multiple data containers.
+- When JIT is available, codesign is entirely bypassed, no need to sign your apps before installing. Otherwise, your app will be signed with the same certificate used by LiveContainer.
+
+Learn more about LiveContainer [here](https://livecontainer.github.io/docs/intro).
+
+## SideStore
+SideStore is an *untethered, community driven* alternative app store for non-jailbroken iOS devices.
+
+SideStore is an iOS application that allows you to sideload apps onto your iOS device with just your Apple ID. SideStore resigns apps with your personal development certificate, and then uses a [specially designed VPN](https://github.com/jkcoxson/em_proxy) in order to trick iOS into installing them. SideStore will periodically "refresh" your apps in the background, to keep their normal 7-day development period from expiring.
+
+SideStore's goal is to provide an untethered sideloading experience. It's a community driven fork of [AltStore](https://github.com/rileytestut/AltStore), and has already implemented some of the community's most-requested features.
+
+Learn more about SideStore [here](https://sidestore.io/).
+
+## Licenses
+* LiveContainer is licensed under [Apache License 2.0](./LICENSE.APACHE). 
+* SideStore is licensed under [GNU Affero General Public License, version 3 only](./LICENSE.AGPL).
+* The code original to this repository is licensed under [the Unlicense](./UNLICENSE).
---
+++ index.js
@@ -0,0 +1,83 @@
+const fs = require('node:fs');
+const { createHash } = require('node:crypto');
+
+fetch('https://sidestore.io/apps-v2.json/')
+.then((res) => res.text())
+.then((text) => fs.writeFileSync('./side.json', text, 'utf-8'))
+var sideJSON = JSON.parse(fs.readFileSync('./side.json', 'utf-8'));
+fetch('https://raw.githubusercontent.com/LiveContainer/LiveContainer/refs/heads/main/apps.json')
+.then((res) => res.text())
+.then((text) => fs.writeFileSync('./lc.json', text, 'utf-8'))
+var lcJSON = JSON.parse(fs.readFileSync('./lc.json', 'utf-8'));
+fetch('https://github.com/LiveContainer/LiveContainer/releases/download/nightly/apps_nightly.json')
+.then((res) => res.text())
+.then((text) => fs.writeFileSync('./lcn.json', text, 'utf-8'))
+var lcnJSON = JSON.parse(fs.readFileSync('./lcn.json', 'utf-8'));
+fetch('https://github.com/suprdratts/LiveContainer/releases/download/nightly/apps_nightly.json')
+.then((res) => res.text())
+.then((text) => fs.writeFileSync('./lcnfork.json', text, 'utf-8'))
+var lcnforkJSON = JSON.parse(fs.readFileSync('./lcnfork.json', 'utf-8'));
+fetch(lcnforkJSON['apps'][0]['downloadURL'])
+.then((res) => res.arrayBuffer())
+.then((data) => { 
+    var hash = createHash('sha256').update(Buffer.from(data)).digest('hex'); 
+    console.log(hash);
+
+    var srcJSON = {}
+    srcJSON['name'] = "LiveContainer + SideStore (nightly)";
+    srcJSON['identifier'] = "com.SideStore.SideStore";
+    srcJSON['website'] = 'https://github.com/suprdratts/LCSideStore';
+    srcJSON['subtitle'] = 'LiveContainer + SideStore';
+    srcJSON['description'] = "This is an unofficial AltStore Source for LiveContainer + SideStore (nightly).\n\n Run iOS apps without actually installing them!"
+    srcJSON['tintColor'] = "#0784FC";
+    srcJSON['iconURL'] = "https://raw.githubusercontent.com/suprdratts/LiveContainer/main/screenshots/AppIcon1024.png";
+    srcJSON['headerURL'] = "https://raw.githubusercontent.com/suprdratts/LiveContainer/main/screenshots/header.png";
+    srcJSON['apps'] = lcnforkJSON['apps'];
+    srcJSON['apps'][0]['name'] = "LiveContainer + SideStore (nightly)";
+    srcJSON['apps'][0]['date'] = srcJSON['apps'][0]['versionDate'];
+    srcJSON['apps'][0]['developerName'] = "LiveContainer Team + SideStore Team";
+    srcJSON['apps'][0]['screenshotURLs'] = srcJSON['apps'][0]['screenshotURLs'].concat(sideJSON['apps'][0]['screenshotURLs']);
+    srcJSON['apps'][0]['sha256'] = hash;
+    srcJSON['apps'][0]['headline'] = 'This is a reminder that this is a third-party LiveContainer and SideStore build'
+    srcJSON['apps'][0]['versions'][0]['headline'] = 'This is a reminder that this is a third-party LiveContainer and SideStore build'
+    srcJSON['apps'][0]['versionDescription'] = `This is a reminder that this is a third-party LiveContainer and SideStore build. You will not receive support for these builds: no one from the LiveContainer team or the SideStore team will help you.
+
+Description from SideStore ${sideJSON['apps'][0]['releaseChannels'][0]['releases'][0]['version']}:
+${sideJSON['apps'][0]['releaseChannels'][0]['releases'][0]['localizedDescription']}
+
+Description from LiveContainer ${lcnJSON['apps'][0]['version']} (${lcnJSON['apps'][0]['commit']}):
+${lcnJSON['apps'][0]['versions'][0]['versionDescription']}
+    `
+    srcJSON['apps'][0]['versions'][0]['localizedDescription'] = srcJSON['apps'][0]['versionDescription']
+
+    srcJSON['apps'][0]['versions'][0]['releaseChannels'] = []
+    var releaseChannel = {
+        "releases": [
+            {
+                "version": srcJSON['apps'][0]['versions'][0]['version'],
+                "date": srcJSON['apps'][0]['versionDate'],
+                "localizedDescription": srcJSON['apps'][0]['versionDescription'],
+                "downloadURL": srcJSON['apps'][0]['downloadURL'],
+                "size": srcJSON['apps'][0]['size'],
+                "sha256": hash
+            }
+        ]
+    }
+    srcJSON['apps'][0]['versions'][0]['releaseChannels'].push(releaseChannel);
+    srcJSON['apps'][0]['permissions'] = sideJSON['apps'][0]['permissions'];
+    let appEntitlements = []
+    appEntitlements = appEntitlements.concat(srcJSON['apps'][0]['appPermissions']['entitlements'])
+    sideJSON['apps'][0]['appPermissions']['entitlements'].forEach(entitlement => { appEntitlements.push(entitlement['name']) });
+    appEntitlements = [...new Set(appEntitlements)];
+    srcJSON['apps'][0]['appPermissions']['entitlements'] = []
+    appEntitlements.forEach(entitlement => {
+        srcJSON['apps'][0]['appPermissions']['entitlements'].push({
+            "name": entitlement
+        })
+    })
+    srcJSON['news'] = sideJSON['news'];
+    srcJSON['news'] = srcJSON['news'].concat(lcJSON['news']);
+    srcJSON['news'] = srcJSON['news'].concat(lcnJSON['news']);
+    srcJSON['apps'][0]['versions'][0]['releaseChannels'][0]['track'] = 'nightly';
+    fs.writeFileSync('./src.json', JSON.stringify(srcJSON, null, 4), 'utf-8')
+})
